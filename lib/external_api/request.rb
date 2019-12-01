d3b3a8d0ba80d64bf0512a6c1361e12d89ec3efb
Nathan Smith <natesmith.copper@gmail.com>
2019-12-01 13:21:19 -0700

    renaming services

--- lib/external_api/request.rb
+++ lib/external_api/request.rb
@@ -8,7 +8,7 @@ class Request
     def get_json(root_path, query = {})
       query_string = query.map{|k, v| "#{k}=#{v}"}.join("&")
       path = query.empty?? root_path : "#{root_path}?#{query_string}"
-      response = api.get(path) # faraday method
+      response = api.get(path)
       [JSON.parse(response.body), response.status]
     end
 
@@ -17,8 +17,12 @@ class Request
       response.merge(error)
     end
 
-    def api
-      Connection.api
+    def api(source)
+      if source == "alpha_vantage"
+        Connection.alpha_vantage
+      else
+        Connection.tradier
+      end
     end
   end
 end
